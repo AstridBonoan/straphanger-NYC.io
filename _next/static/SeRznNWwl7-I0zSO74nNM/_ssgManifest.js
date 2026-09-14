aaa29cc2e00766029428bf3127dc932a5e5d9617
AstridBonoan <AstridBonoan@users.noreply.github.com>
2026-09-14 18:35:14 +0000

    deploy: 7adad1d661074f71b5e4302c1d58a194004d2df8

---
+++ _next/static/SeRznNWwl7-I0zSO74nNM/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Faccount\u002Forders\u002F[id]","\u002Fadmin\u002Fcustomers\u002F[id]","\u002Fadmin\u002Forders\u002F[id]","\u002Fadmin\u002Fproducts\u002F[id]\u002Fedit","\u002Fproduct\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
